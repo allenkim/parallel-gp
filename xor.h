c2f56ba50f131d28f1b3311acb52a830aa2a2d69
Abi <abiyaz.chowdhury@stonybrook.edu>
2018-04-06 16:52:07 +0100

    Added constructor for node and part of graph generation function

--- xor.h
+++ xor.h
@@ -10,7 +10,16 @@ enum NonTerminal{
 	NOT
 }
 
+int num_terminal_types = 2;
+int num_nonterminal_types = 5;
+
+int num_arguments(NonTerminal non_terminal_type){
+	if (non_terminal_type != NonTerminal.NOT){
+		return 2;
+	}
+	return 1;
+}
+
 Terminal microeval(NonTerminal id);
 float fitness();
 Terminal eval();
-
